d32e392be6136608d20a754e08caee2b252a7eac
huckbee <huckbrock@gmail.com>
2015-09-15 09:40:49 -0500

    adds getRousources, displayCards, and diplayCategory

---
+++ client/app.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+var name = "",
+    logo = "",
+    embedLink = "",
+    howToVideo = "",
+    description = "",
+    category = "",
+    subject = "",
+    tags = [];
+
+function getResources () {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: TBD,
+        success: displayCards(data)
+    })
+}
+
+function diplayCards (data){
+    for (var i = 0; i < data.length; i++){
+        name = data[i].name;
+        logo = data[i].logo;
+        embedLink = data[i].embedLink;
+        howToVideo = data[i].howToVideo;
+        description = data[i].description;
+        category = data[i].category;
+        subject = data[i].subject;
+        tags = data[i].tags;
+        var $el =
+        $('#cardContainer').append()
+    }
+}
+
+function displayCategory (data, category){
+    categoryArray = [];
+    for (var i = 0; i < data.length; i++){
+        if(data.[i].category === category){
+            categoryArray.push(data[i]);
+        }
+    }
+    return categoryArray;
+}
+
