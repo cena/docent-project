66a50777162981300ebc51abad2525799b88c55b
huckbee <huckbrock@gmail.com>
2015-09-23 17:14:02 -0500

    adds admin.js to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,12 @@ module.exports = function(grunt){
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             client: {
-                src: ['client/scripts/app.js',
-                    'client/scripts/admin.js'
-                    ],
-                dest: 'server/public/assets/scripts/app.min.js'
+                src: 'client/scripts/app.js',
+                dest: 'server/public/assets/scripts/app.min.js',
+            },
+            adminjs: {
+                src: 'client/scripts/admin.js',
+                dest: 'server/public/assets/scripts/admin.min.js',
             }
         },
         copy: {
