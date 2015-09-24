60e84019f56b8c179e0a984287b0fd2e9af4ba25
Skyler <skyler.m.degrote@outlook.com>
2015-09-24 09:39:48 -0500

    table showing up on admin page

--- client/scripts/admin.js
+++ client/scripts/admin.js
@@ -16,37 +16,49 @@ $(document).ready(function() {
         $("#newModal").modal('show');
     });
 
-
-    displayAdmin();
+    getResources();
 
 });
 
+function  getResources() {
+    filteredArray=[];
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: "/resources",
+        success: function(data) {
+            //data.resources.sort(compareAlphabetically);
+            //callback(data.resources);
+            console.log(data);
+            displayAdmin(data);
+
+        }
+    })
+}
+
 // ADMIN PAGE CONTENT
 function displayAdmin (data){
-    for(var i = 0; i<data; i++) {
-        console.log(data);
+    console.log(data);
+    for(var i = 0; i < data.resources.length; i++) {
         tags = "";
-        embedName = data[i].embedName;
-        logo = data[i].logo;
-        embedLink = (data[i].embedLink) ? data[i].embedLink : data[i].embedName;
-        howto = (data[i].howto) ? data[i].howto : "";
-        description = (data[i].description) ? data[i].description : descriptionPlaceholder;
-        category = (data[i].category) ? data[i].category : "";
-        subject = (data[i].subject) ? "" : data[i].embedName;
-        if (data[i].tags !== null && data[i].tags !== 0) {
-            for (var j = 0; j < data[i].tags.length; j++) {
-                tags += '<p class="tag">' + data[i].tags[j] + '</p>';
+        embedName = data.resources[i].embedName;
+        logo = data.resources[i].logo;
+        embedLink = (data.resources[i].embedLink) ? data.resources[i].embedLink : "";
+        howto = (data.resources[i].howto) ? data.resources[i].howto : "";
+        description = (data.resources[i].description) ? data.resources[i].description : "";
+        category = (data.resources[i].category) ? data.resources[i].category : "";
+        subject = (data.resources[i].subject) ? data.resources[i].subject : "";
+        if (data.resources[i].tags !== null && data.resources[i].tags !== 0) {
+            for (var j = 0; j < data.resources[i].tags.length; j++) {
+                tags += '<p class="tag">' + data.resources[i].tags[j] + '</p>';
             }
         }
-        //var adminTable = '<table id="adminTable"></table>';
-        //var tableHead = '<th>Embed Name</th><th>Logo Link</th><th>Embed Link</th><th>How To Video</th><th>Description</th><th>Category</th><th>Subjects</th><th>Tags</th><th>Edit and Delete</th>';
-        //var tableRow = '<tr></tr>';
-        //
-        //$("#admin").append(adminTable);
-        //$(adminTable).append(tableHead + tableRow);
-        //$(tableRow).append('<td>' + embedName + '</td><td>' + logo + '</td><td class="embedlink">' + embedLink + '</td><td class="howtolink">' + howto + '</td><td class="description">' + description + '</td><td>' + category + '</td><td>' + subject + '</td><td>' + tags + '</td>')
-        $("body").append("something");
+        var tableHead = '<th>Embed Name</th><th>Logo Link</th><th>Embed Link</th><th>How To Video</th><th>Description</th><th>Category</th><th>Subjects</th><th>Tags</th><th>Edit and Delete</th>';
+        var tableRow = '<tr></tr>';
 
+        $("#admin").append('<tr><td>' + embedName + '</td><td>' + logo + '</td><td class="embedlink">' + embedLink + '</td><td class="howtolink">' + howto + '</td><td class="description">' + description + '</td><td>' + category + '</td><td>' + subject + '</td><td>' + tags + '</td></tr>')
+
+        console.log("working?")
     }
 
 }
